6d193c4542691d1f860b74be9f03e20f8a7891ee
Liemeon <lgh029@naver.com>
2018-10-16 16:56:59 +0900

    add turbomode

--- myapp.c
+++ myapp.c
@@ -2,9 +2,21 @@
 #include "func2.h"
 #include <stdio.h>
 
+void turbo_on()
+{
+  printf("turbo_on\n");
+}
+
+void turbo_off()
+{
+  printf("turbo_off\n");
+}
+
 int main()
 {
+  turbo_on():
   func1();
   func2();
+  turbo_off();
   return 0;
 }
