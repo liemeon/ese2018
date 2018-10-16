a074ab15c302449a34ed5e984dfeea72f85b02c1
Liemeon <lgh029@naver.com>
2018-10-16 16:44:44 +0900

    fuck exam

---
+++ func2.c
@@ -0,0 +1,7 @@
+#include "func2.h"
+#include <stdio.h>
+
+void foo2()
+{
+  printf("hoo..");
+}
--- myapp.c
+++ myapp.c
@@ -1,8 +1,10 @@
 #include "func1.h"
+#include "func2.h"
 #include <stdio.h>
 
 int main()
 {
   func1();
+  func2();
   return 0;
 }
