bf45e9bdee1f3f974f53c874a4ebf71e21b2f3ef
Liemeon <lgh029@naver.com>
2018-10-16 17:01:47 +0900

    postprocessing!

--- myapp.c
+++ myapp.c
@@ -7,10 +7,16 @@ void preprocess()
   printf("preprocessing!\n");
 }
 
+void postprocess()
+{
+  printf("postprocessing!\n");
+}
+
 int main()
 {
   preprocess();
   func1();
   func2();
+  postprocess();
   return 0;
 }
